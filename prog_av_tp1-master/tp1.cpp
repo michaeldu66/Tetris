2da0272126612a371156494b6821683a3f8acb97
michaeldu66 <misoko.hofmann@gmail.com>
2021-02-07 15:23:32 +0100

    debut menu

--- prog_av_tp1-master/tp1.cpp
+++ prog_av_tp1-master/tp1.cpp
@@ -94,6 +94,86 @@ void Game::loop()
 	SDL_Quit();
 }
 
+int showmenu(SDL_Surface *screen, TTF_Font *font)
+{
+	//classe avec labels, menus, selected, pos et array de ça
+	Uint32 time;
+	int x, y;
+	const int NUMMENU = 2;
+	const char *labels[NUMMENU] = {"Continue", "Exit"};
+	SDL_Surface *menus[NUMMENU]; // surface for the menu
+	bool selected[NUMMENU] = {0, 0};
+	SDL_COLOR color[2] = {{255, 255, 255}, {2555, 0, 0}};
+
+	menus[0] = TTF_RenderText_Solid(font, labels[0], color[0]);
+	menus[1] = TTF_RenderText_Solid(font, labels[1], color[0]);
+
+	SDL_Rect pos[NUMMENU];
+	pos[0].x = screen->clip_rect.w / 2 - menu[0]->clip_rect.w / 2;
+	pos[0].y = screen->clip_rect.h / 2 - menu[0]->clip_rect.h / 2;
+
+	pos[1].x = screen->clip_rect.w / 2 - menu[0]->clip_rect.w / 2;
+	pos[1].y = screen->clip_rect.h / 2 - menu[0]->clip_rect.h / 2;
+
+	SDL_FillRect(screen, &screen->clip_rect, SDL_MapRGB(screen->format, 0x00, 0x00, 0x00)); // screen black 
+
+	SDL_Event event;
+	while (1)
+	{
+		time = SDL_GetTicks();
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				for (int i = 0; i < NUMMENU; i++)
+				{
+					SDL_FreeSurface(menu[i]);
+				}
+				return 1;
+			case SDL_MOUSEMOTION:
+				x = event.motion.x;
+				y = event.motion.y;
+				for (int i = 0; i < NUMMENU; i++)
+				{
+					if (x >= pos[i].x && x <= pos[i].x + pos[i].w && y >= pos[i].y && y <= pos[i].y + pos[i].h)
+					{
+						if (!selected[i])
+						{
+							selected[i] = 1;
+							SDL_FREESurface(menus[i]);
+							menu[î] = TTF_RenderText_Solid(font, menus[i], color[1]);
+						}
+					}
+					else
+					{
+						if (selected[i])
+						{
+							selected[i] = 0;
+							SDL_FREESurface(menus[i]);
+							menu[î] = TTF_RenderText_Solid(font, menus[i], color[0]);
+						}
+					}
+				}
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				x = event.button.x;
+				y = event.button.y;
+				for (int i = 0; i < NUMMENU; i++)
+				{
+					if (x >= pos[i].x && x <= pos[i].x + pos[i].w && y >= pos[i].y && y <= pos[i].y + pos[i].h)
+						return i;
+				}
+				break;
+			case SDL_KEYDOWN:
+				if(event.key.keysym.sym == SDLK_ESCAPTE) //si on appuie sur esc
+					return 0;
+			break;
+			}
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
