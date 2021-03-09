0b6d06f0b643dd925d3f548c9b31bafd38735e3d
michaelof66 <michael.hofmann@outlook.fr>
2021-03-09 23:20:40 +0100

    effacement main(ancienne version)

--- README.md
+++
@@ -1,7 +0,0 @@
-# Tetris*
-
-Pour compiler et exécuter le projet:
-cd prog_av_tp1-master/build
-make clean
-make
-./TP_CPP_SDL
--- prog_av_tp1-master/sprite.cpp
+++
@@ -1,7 +0,0 @@
-#include "sprite.h"
-
-
-Sprite::Sprite(const char* link_sprite):Surface(){
-    this->set_surf(link_sprite);
-    cout << "Sprite créé" << endl;
-}
